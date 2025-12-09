9d9482ffd2d431da20a1a595ba294e35787043f3
bw8686 <51877146+bw8686@users.noreply.github.com>
2025-12-09 15:12:51 +0000

    Initial commit

---
+++ app/src/main/java/uk/bw86/nscgschedule/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package uk.bw86.nscgschedule.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun NSCGScheduleTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
